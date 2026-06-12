4b8a2c418b30e7b3e80bb5fb72528beb76aa9c6b
kimegami_shinji <workshinji2023@gmail.com>
2026-06-12 21:17:21 +0900

    タグAPIルートを追加

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\AdminTagController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LivePostController;
 use App\Http\Controllers\Api\PasswordResetController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/lives', [LivePostController::class, 'index']);
 Route::get('/lives/{id}', [LivePostController::class, 'show']);
+
+Route::get('/tags', [TagController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -33,8 +37,14 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/dashboard', [AdminController::class, 'dashboard']);
         Route::get('/users', [AdminController::class, 'users']);
         Route::delete('/users/{id}', [AdminController::class, 'deleteUser']);
+
         Route::get('/lives', [AdminController::class, 'lives']);
         Route::post('/lives/{id}', [AdminController::class, 'updateLive']);
         Route::delete('/lives/{id}', [AdminController::class, 'deleteLive']);
+
+        Route::get('/tags', [AdminTagController::class, 'index']);
+        Route::post('/tags', [AdminTagController::class, 'store']);
+        Route::post('/tags/{id}', [AdminTagController::class, 'update']);
+        Route::delete('/tags/{id}', [AdminTagController::class, 'destroy']);
     });
 });
